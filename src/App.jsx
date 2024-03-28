8c9ecfda95f6e76b5128874c417871458eb1a212
idsab <idasabic@gmail.com>
2024-03-28 17:17:08 +0100

    folder structure test

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { TopSection } from "./Components/TopSection/TopSection"
+import { TechSection } from "./Components/TechSection/TechSection"
+import { ProjectSection } from "./Components/ProjectSection/ProjectSection"
+import { MyWordsSection } from "./Components/MyWordsSection/MyWordsSection"
+import { SkillsSection } from "./Components/SkillsSection/SkillsSection"
+import { ContactSection } from "./Components/ContactSection/ContactSection"
+import "./App.css"
+
+export const App = () => {
+
+  return (
+    <>
+    <TopSection />  
+    <TechSection />  
+    <ProjectSection />  
+    <MyWordsSection />  
+    <SkillsSection />  
+    <ContactSection />
+    </>
+  )
+}
+
+
